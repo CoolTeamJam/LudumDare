6b584da7784063dc9bb15bbc684e16a06570e117
Alexandre Bourse <alexandre.bourse@gmail.com>
2024-04-13 11:51:11 -0400

    Add Animation for buttons

--- Assets/UI/MainMenu.cs
+++ Assets/UI/MainMenu.cs
@@ -6,15 +6,51 @@ using UnityEngine.UIElements;
 
 public class MainMenu : MonoBehaviour
 {
-    public VisualElement startButton;
+    private VisualElement startButton;
+    private VisualElement settingsButton;
+    private VisualElement creditsButton;
+    private VisualElement exitButton;
+
     private void Start()
     {
         startButton = GetComponent<UIDocument>().rootVisualElement.Q("StartButton");
         startButton.RegisterCallback<ClickEvent>(OnClickStartButton);
+
+        settingsButton = GetComponent<UIDocument>().rootVisualElement.Q("SettingsButton");
+        settingsButton.RegisterCallback<ClickEvent>(OnClickSettingsButton);
+
+        creditsButton = GetComponent<UIDocument>().rootVisualElement.Q("CreditsButton");
+        creditsButton.RegisterCallback<ClickEvent>(OnClickCreditsButton);
+
+        exitButton = GetComponent<UIDocument>().rootVisualElement.Q("ExitButton");
+        exitButton.RegisterCallback<ClickEvent>(OnClickExitButton);
     }
+
 
     private void OnClickStartButton(ClickEvent evt)
     {
         SceneManager.LoadScene(1);
     }
+
+    private void OnClickSettingsButton(ClickEvent evt)
+    {
+        Debug.Log("Settings");
+    }
+
+    private void OnClickCreditsButton(ClickEvent evt)
+    {
+        Debug.Log("Credits");
+    }
+
+    private void OnClickExitButton(ClickEvent evt)
+    {
+        Debug.Log("Exit");
+        Application.Quit();
+    }
+
+    
+
+   
+
+   
 }
