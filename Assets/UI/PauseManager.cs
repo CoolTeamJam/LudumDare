c0b8d313343c5e77f7885fc54427eab14dddac38
Alexandre Bourse <alexandre.bourse@gmail.com>
2024-04-13 17:56:40 -0400

    Fix Cursor Issue

--- Assets/UI/PauseManager.cs
+++ Assets/UI/PauseManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,15 +6,52 @@ using UnityEngine.UIElements;
 
 public class PauseManager : MonoBehaviour
 {
+    private readonly string activeClass = "pause-active";
+
     [SerializeField] private UIDocument UiDocument;
     private VisualElement rootElement;
     private VisualElement pauseElement;
-    private string activeClass = "pause-active";
+    private VisualElement resumeButton;
+    private VisualElement settingsButton;
+    private VisualElement quitButton;
 
-    private void OnEnable()
+
+    private void Start()
     {
         rootElement = UiDocument.rootVisualElement;
         pauseElement = rootElement.Q(className: "pause");
+
+        resumeButton = rootElement.Q("resumeButton");
+        resumeButton.RegisterCallback<ClickEvent>(OnClickResumeButton);
+
+        settingsButton = rootElement.Q("settingsButton");
+        settingsButton.RegisterCallback<ClickEvent>(OnClickSettingsButton);
+
+        quitButton = rootElement.Q("quitButton");
+        quitButton.RegisterCallback<ClickEvent>(OnClickQuitButton);
+    }
+
+
+    private void OnEnable()
+    {
+        
+
+    }
+
+    private void OnClickQuitButton(ClickEvent evt)
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+    private void OnClickSettingsButton(ClickEvent evt)
+    {
+        Debug.Log("Settings");
+    }
+
+    private void OnClickResumeButton(ClickEvent evt)
+    {
+        Close();
     }
 
     private void Update()
@@ -31,12 +69,16 @@ public class PauseManager : MonoBehaviour
     private void Open()
     {
         pauseElement.AddToClassList(activeClass);
+        UnityEngine.Cursor.visible = true;
+        UnityEngine.Cursor.lockState = CursorLockMode.Confined;
         Time.timeScale = 0;
     }
 
     private void Close()
     {
         pauseElement.RemoveFromClassList(activeClass);
+        UnityEngine.Cursor.visible = true;
+        UnityEngine.Cursor.lockState = CursorLockMode.Locked;
         Time.timeScale = 1;
     }
 
